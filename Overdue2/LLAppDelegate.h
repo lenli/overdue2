c2ec3b2bdeb5d04ed922f6d636ffe78ca02287a4
Len Li <leonard.li@gmail.com>
2013-12-02 21:42:28 -0500

    Initial Commit

---
+++ Overdue2/LLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LLAppDelegate.h
+//  Overdue2
+//
+//  Created by Len on 12/2/13.
+//  Copyright (c) 2013 LL inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Overdue2/LLViewController.m
@@ -0,0 +1,29 @@
+//
+//  LLViewController.m
+//  Overdue2
+//
+//  Created by Len on 12/2/13.
+//  Copyright (c) 2013 LL inc. All rights reserved.
+//
+
+#import "LLViewController.h"
+
+@interface LLViewController ()
+
+@end
+
+@implementation LLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
